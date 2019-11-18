b22a7f925a4bebcdf8b3a3e78d9107268673c61a
quixoticrose007 <57918044+quixoticrose007@users.noreply.github.com>
2019-11-18 12:40:09 -0800

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+#var assigned string value
+subject = "Hallmarklabs' Lunch and Learn!"
+
+#Get user name
+user_name =input("What is your name, LOSER?")
+
+#Concat string + vars together
+welcome_message = "Welcome "+ user_name+' to '+subject+" (Just kidding before - you're not a loser, probably.)"
+
+#Display message! It learned the name!
+print(welcome_message)
